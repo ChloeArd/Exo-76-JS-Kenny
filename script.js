d4392215ab1d7756a976dff648f0b13e8de076e1
ChloeArd <chloe.ardoise@gmail.com>
2020-12-04 13:13:46 +0100

    Exercice 76

--- script.js
+++ script.js
@@ -5,3 +5,26 @@ document.getElementById('up').addEventListener('click',function() {
     t = t - 10;
     document.getElementById('kenny').style.top = t + 'px';
 });
+
+document.getElementById('left').addEventListener('click',function() {
+    let t = parseInt(document.getElementById('kenny').style.left);
+    t = t - 10;
+    document.getElementById('kenny').style.left = t + 'px';
+});
+
+document.getElementById('right').addEventListener('click',function() {
+    let t = parseInt(document.getElementById('kenny').style.left);
+    t = t + 10;
+    document.getElementById('kenny').style.left = t + 'px';
+});
+
+document.getElementById('down').addEventListener('click',function() {
+    let t = parseInt(document.getElementById('kenny').style.top);
+    t = t + 10;
+    document.getElementById('kenny').style.top = t + 'px';
+});
+
+let bordure = document.getElementById('viewport');
+
+
+
