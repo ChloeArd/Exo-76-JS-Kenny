a2aaea26f75e63c088aa2d5cb10b307309ffacea
ChloeArd <chloe.ardoise@gmail.com>
2020-12-05 12:43:48 +0100

    Exercice 76

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-
+// Etape 1: Déplacement de Kenny en cliquant sur les inputs.
 document.getElementById('up').addEventListener('click',function() {
     let t = parseInt(document.getElementById('kenny').style.top);
     t = t - 10;
@@ -24,16 +24,18 @@ document.getElementById('down').addEventListener('click',function() {
     document.getElementById('kenny').style.top = t + 'px';
 });
 
+
 let bordure = document.getElementById('viewport');
 let kenny = document.getElementById('kenny');
 
+//Etape 2: Conditions pour empécher kenny de sortir des bordures.
 if (kenny === bordure) {
 
 }
 
 
 
-
+// Etape 3: Zone Rouge.
 let zoneRouge = document.createElement('div');
 zoneRouge.id = "zoneRouge";
 zoneRouge.style.height = "50px";
@@ -44,7 +46,7 @@ zoneRouge.style.bottom = "100px";
 bordure.append(zoneRouge);
 
 
-
+// Etape 4 et Etape 5: Message d'alerte en disant que kenny est mort et le replacer.
 if (kenny === zoneRouge) {
     alert("Kenny est mort");
     kenny.style.top = 200 + 'px';
@@ -52,8 +54,10 @@ if (kenny === zoneRouge) {
 
 }
 
+//Etape 6: Nombre de fois que kenny est mort.
 
 
 
-keyup
+//Etape 7: Clavier pour déplacer kenny.
+// key
 
